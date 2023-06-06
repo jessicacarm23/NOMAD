d82264f01ff8831338b10d7cfd4de7c0da4f5438
Jessica Carvajal <jessicacarm23@gmail.com>
2023-06-06 20:34:30 +0100

    adde the title to the article and started with the show page for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/Home", to: "pages#home"
-  get "/:city", to: "pages#show"
+  get "/Home", to: "articles#topic"
+  get "/Home?query=:city", to: "pages#city"
+  get "/:city", to: "pages#city"
   resources :article, only: [:show, :new, :create, :index, :destroy]
   resources :saved_articles, only: [:create, :index, :destroy]
   resources :chat_rooms, only: [:create, :show, :destroy]
   resources :users, only: [:edit]
-  get ":city/articles/topics", to: "articles#topic"
+  get ":city/articles/topics", to: "articles#topic", as: "city_articles_topic"
   get "/profile", to: "pages#profile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
