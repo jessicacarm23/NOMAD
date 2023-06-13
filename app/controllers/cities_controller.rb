df229bed3e5d046a0a36c7ded32ea3d09c9cfb72
Jamie Ewing <jamesewing0@gmail.com>
2023-06-13 21:27:46 +0100

    attempt 2

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -20,19 +20,6 @@ class CitiesController < ApplicationController
 
 
 
-def map_image
-  access_token = 'pk.eyJ1IjoiamFtZXNld2luZzAiLCJhIjoiY2xmbGlreGFrMDJhOTN3cjJ1anVhN2xpYSJ9.gB6KxglmzVc06uUVc2BkFQ'
-  style = 'mapbox://styles/mapbox/streets-v11'
-  zoom = 12
-  lat = 37.7749
-  lon = -122.4194
-  width = 800
-  height = 600
 
-  map_url = "https://api.mapbox.com/styles/v1/#{style}/static/#{lon},#{lat},#{zoom}/#{width}x#{height}?access_token=#{access_token}"
-  @map_image_url = HTTParty.get(map_url).body
-
-  render 'your_view_file'
-end
 
 end
