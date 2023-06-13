400d4f47c630d7ac15a2f3cf7bbaea975cb5a093
Jessica Carvajal <jessicacarm23@gmail.com>
2023-06-13 17:58:27 +0100

    added the hearts for the saved articles, fixed the double search bar, rereouted some articles, tried to improve the seed, worked in front end for the cards

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,5 +1,6 @@
 class CitiesController < ApplicationController
   def show
+    @city_name = params[:query]
     @city_articles = Article.search_by_city(params[:query])
     @topics = Article.all.distinct.pluck(:topic)
   end
