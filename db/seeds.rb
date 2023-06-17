cb9624a2052300bee1e47de473ffb42ffaf29a39
Belen Martinez <belenmgil@gmail.com>
2023-06-17 14:51:34 +0100

    adding some styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ User.create!(
   email: 'test@test.com',
   password: 'password',
   name: "Jessica",
-  passport: "Colombian"
+  passport: "Colombian",
+  bio: "I love remote work as a developer",
   city: "London"
-  bio: "I love remote work as a developer"
 )
 
 user1 = User.create(name: "Jasmine", passport: "French", email: "jasmine@test.com", city: "London", bio: "A nice person", password: 'password')
