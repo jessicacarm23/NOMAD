ba36c2583f101e67a4ce90dc1e55278ad9e6e033
Jasmine Thakral <jasminethakral@gmail.com>
2023-06-17 15:44:12 +0100

    chat working

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,37 +1,37 @@
-import { Controller } from "@hotwired/stimulus"
-
+import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static values = {
     apiKey: String,
     markers: Array
   }
-
   connect() {
     console.log("hi from map")
     mapboxgl.accessToken = this.apiKeyValue
-
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
-
-    // this.#addMarkersToMap()
-    // this.#fitMapToMarkers()
+     this.#addMarkersToMap()
+     this.#fitMapToMarkers()
   }
-
-  // #addMarkersToMap() {
-  //   this.markersValue.forEach((marker) => {
-  //     const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-  //     new mapboxgl.Marker()
-  //       .setLngLat([ marker.lng, marker.lat ])
-  //       .setPopup(popup)
-  //       .addTo(this.map)
-  //   })
-  // }
-
-  // #fitMapToMarkers() {
-  //   const bounds = new mapboxgl.LngLatBounds()
-  //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-  //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  // }
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
+      const element = document.createElement('div');
+      element.className = 'marker';
+      const markerObj = new mapboxgl.Marker(element)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map);
+      // Open the corresponding show.html.erb file when the marker is clicked
+      element.addEventListener('click', () => {
+        window.location.href = `show.html.erb`;
+      });
+    });
+  }
+   #fitMapToMarkers() {
+     const bounds = new mapboxgl.LngLatBounds()
+     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+   }
 }
