0cde469aa55f64314bbd422e9bc6858e7bbbc1cd
Jasmine Thakral <jasminethakral@gmail.com>
2023-06-17 12:53:04 +0100

    working on chat feature

--- app/javascript/controllers/chat_room_subscription_controller.js
+++ app/javascript/controllers/chat_room_subscription_controller.js
@@ -3,14 +3,28 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chat-room-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
   static targets = ["messages"]
+  static values = { chatroomId: Number }
+
   connect() {
+    console.log("testing if this works")
+
+
     this.channel = createConsumer().subscriptions.create(
-      { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => console.log(data) }
+      { channel: "ChatRoomChannel", id: this.chatroomIdValue },
+      {received: (data) => {this.#insertMessageAndScrollDown(data)}}
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  resetForm(event) {
+    console.log(event);
+    event.target.reset()
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
 }
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -17,38 +17,38 @@ export default class extends Controller {
   }
 }
 
-connect() {
-  // [...]
-  this.#addMarkersToMap()
-}
+// connect() {
+//   // [...]
+//   this.#addMarkersToMap()
+// }
 
-#addMarkersToMap() {
-  this.markersValue.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(this.map)
-  })
-}
+// #addMarkersToMap() {
+//   this.markersValue.forEach((marker) => {
+//     new mapboxgl.Marker()
+//       .setLngLat([ marker.lng, marker.lat ])
+//       .addTo(this.map)
+//   })
+// }
 
-connect() {
-  // [...]
-  this.#addMarkersToMap()
-  this.#fitMapToMarkers()
-}
+// connect() {
+//   // [...]
+//   this.#addMarkersToMap()
+//   this.#fitMapToMarkers()
+// }
 
 // [...]
-#fitMapToMarkers() {
-  const bounds = new mapboxgl.LngLatBounds()
-  this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-}
+// #fitMapToMarkers() {
+//   const bounds = new mapboxgl.LngLatBounds()
+//   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+//   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+// }
 
-#addMarkersToMap() {
-  this.markersValue.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup) // Add this
-      .addTo(this.map)
-  });
-}
+// #addMarkersToMap() {
+//   this.markersValue.forEach((marker) => {
+//     const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
+//     new mapboxgl.Marker()
+//       .setLngLat([ marker.lng, marker.lat ])
+//       .setPopup(popup) // Add this
+//       .addTo(this.map)
+//   });
+// }
