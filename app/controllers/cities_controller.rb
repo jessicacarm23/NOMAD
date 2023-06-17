8f33fcd277f2981569c38b26c1f1d17c2bcbf604
Belen Martinez <belenmgil@gmail.com>
2023-06-17 16:30:11 +0100

    users profiles

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,5 +5,6 @@ class CitiesController < ApplicationController
     @city_name = params[:query]
     @city_articles = Article.search_by_city(params[:query])
     @topics = Article.all.distinct.pluck(:topic)
+    @users = User.all
   end
 end
