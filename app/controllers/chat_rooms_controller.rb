00555265d786c3d94d82349086cefc714771b3c0
Jasmine Thakral <jasminethakral@gmail.com>
2023-06-20 20:24:56 +0100

    making the chat link to profile

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -3,4 +3,15 @@ class ChatRoomsController < ApplicationController
     @chatroom = ChatRoom.find(params[:id])
     @message = Message.new
   end
+  
+  def create
+    chatroom_initiated_by_current_user = ChatRoom.find_by(user_receiver_id: params[:user_receiver_id], user_initiator_id: current_user.id )
+    chatroom_initiated_by_params_user = ChatRoom.find_by(user_initiator_id: params[:user_receiver_id], user_receiver_id: current_user.id )
+    if chatroom_initiated_by_current_user.present?
+      redirect_to chat_rooms_path(@chatroom)
+    elsif chatroom_initiated_by_params_user.present?
+      redirect_to chat_rooms_path(@chatroom)
+    else
+    end
+  end
 end
